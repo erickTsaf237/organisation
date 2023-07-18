9f0384d23b8e09cbdb3470e08718177d06a28994
erick tsafack <ericktsafack1aff@gmail.com>
2023-07-18 03:55:04 +0100

    a cette etape on peut voter2

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import {HttpLoggerMiddleware} from "./logger/logger.middleware";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+
     origin: ['http://localhost:3000', 'http://192.168.43.19:3000', 'https://organisations.adaptable.app/'],
   });
   app.use(new HttpLoggerMiddleware().getMiddleware());
